123ba3561b1fb1ff37c52dadb6ef5e094bb7f14b
juancruzbupo <juancruzbupo@gmail.com>
2014-06-29 22:38:26 -0300

    blog

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,44 @@
+class PostsController < ApplicationController
+  def index
+  	@post = Post.all 
+  end
+
+  def new
+  	@post = Post.new 
+  end
+
+  def create
+	 @post = Post.new(params[:posts].permit(:title, :body))
+	 if @post.save
+    redirect_to @post
+   else
+    render 'new'
+   end
+	end
+
+  def show
+    @post = Post.find params[:id]
+  end
+  
+  def edit
+    @post = Post.find params[:id]
+  end
+
+  def update
+    @post = Post.find params[:id] 
+    @post.update(post_params)
+    redirect_to post_url(@post)
+  end
+
+  def destroy
+    @post = Post.find params[:id]
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+end
---
+++ db/migrate/20140627180039_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.string :commenter
+      t.text :body
+
+      t.references :posts, index: true
+
+      t.timestamps
+    end
+  end
+end
